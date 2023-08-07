e0833b822f3620121e79225fede11d4b36fd7679
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:10:05 +0200

    Add project files.

---
+++ Using_Properties_and_Fields/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+namespace Using_Properties_and_Fields
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Person p = new Person();
+            p.Age= 65;
+
+            Console.WriteLine("Age :" + p.Age);
+
+        }
+
+
+        public class Person
+        {
+            private int age;
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                   if ((value > 0) && (value <65 ))
+                    {
+
+                        age = value;
+
+                    }
+                    else
+                    {
+
+                        throw new Exception("Age cannot be over 65");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
